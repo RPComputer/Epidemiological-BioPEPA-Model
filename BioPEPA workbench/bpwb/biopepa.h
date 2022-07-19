f5791674329d4462465912f4ff4d62621693d6d5
RPComputer <r.p.computer2@gmail.com>
2022-07-19 17:21:10 +0200

    Repository cleanup

--- BioPEPA workbench/bpwb/biopepa.h
+++
@@ -1,5 +0,0 @@
-// Output by the Bio-PEPA Workbench
-#define TimeFinal 200
-#define Report 7
-#define Iterations 1
-#define ProgressInterval 1
--- BioPEPA workbench/bpwb/examples/rnaModel.plot
+++
@@ -1,1 +0,0 @@
-DNAl1init,El35Stot,El27SAtot,El27SBtot,El20Stot,El18Stot,El25Stot,El58Stot,El7Stot
