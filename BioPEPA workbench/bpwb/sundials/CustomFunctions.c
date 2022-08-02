db35295449d798b3264d565b5d4576d817602e74
RPComputer <r.p.computer2@gmail.com>
2022-08-02 17:46:00 +0200

    Modifiche e funzioni completate per il funzionamento congiunto di stochkit e sundials. Aggiunta configurazione per il debugging di sundials

--- BioPEPA workbench/bpwb/sundials/CustomFunctions.c
+++ BioPEPA workbench/bpwb/sundials/CustomFunctions.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <assert.h>
 
 double *rtValues;
 int first = 1;
 int lines = 0;
 
-int countlines(char *filename)
+int countlines(const char *filename)
 {
   // count the number of lines in the file called filename
   FILE *fp = fopen(filename, "r");
@@ -35,20 +36,24 @@ double readRt(double t, const char *filename, double x)
     1 first execution: read the whole file in a vector of Rt values. The index of the vector is equal to the daytime of the value
     2 access vector at index t and get the element
   */
+  char buffer[100];
   if (first)
   {
     lines = countlines(filename);
     int n = 0;
-    char str1[10], str2[15];
+    char str1[15], str2[20];
     double temp;
+    int check;
     rtValues = (double *)malloc(lines * sizeof(double));
     FILE *fp = fopen(filename, "r");
 
     if (fp == NULL)
       return 0;
+    fgets(buffer, 100, fp); //skip header line
     while (!feof(fp))
     {
-      fscanf(fp, "%s,%s,%lf", str1, str2, temp);
+      check = fscanf(fp, "%[^,],%[^,],%lf", str1, str2, &temp);
+      assert(check == 3 || check == 1);
       rtValues[n] = temp;
       n++;
     }
